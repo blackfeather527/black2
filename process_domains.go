c64ba9cefe73fad90953734ef7127b27759e0200
blackfeather527 <blackfeather527@gmail.com>
2024-10-09 01:56:13 +0800

    Update process_domains.go

--- process_domains.go
+++ process_domains.go
@@ -7,50 +7,30 @@ import (
 )
 
 func main() {
+    // 定义自定义的 FlagSet
+    fs := flag.NewFlagSet("", flag.ContinueOnError)
+    fs.Usage = func() {} // 覆盖默认的 Usage 函数，使其不输出任何内容
+
     // 定义命令行参数
-    var (
-        inputFile  string
-        outputDir  string
-        showHelp   bool
-    )
+    var inputPath string
+    var outputDir string
 
-    // 设置命令行参数
-    flag.StringVar(&inputFile, "i", "domains.txt", "输入文件路径")
-    flag.StringVar(&inputFile, "input", "domains.txt", "输入文件路径")
-    flag.StringVar(&outputDir, "o", ".", "输出文件目录")
-    flag.StringVar(&outputDir, "output", ".", "输出文件目录")
-    flag.BoolVar(&showHelp, "h", false, "显示帮助信息")
-    flag.BoolVar(&showHelp, "help", false, "显示帮助信息")
+    fs.StringVar(&inputPath, "i", "domains.txt", "输入文件路径")
+    fs.StringVar(&inputPath, "input", "domains.txt", "输入文件路径")
+    fs.StringVar(&outputDir, "o", ".", "输出文件目录")
+    fs.StringVar(&outputDir, "output", ".", "输出文件目录")
 
-    // 自定义Usage函数来忽略未定义的参数
-    flag.Usage = func() {
-        fmt.Fprintf(os.Stderr, "Usage of %s:\n", os.Args[0])
-        flag.PrintDefaults()
-    }
-
-    // 解析命令行参数
-    flag.Parse()
-
-    // 显示帮助信息
-    if showHelp {
-        flag.Usage()
-        return
-    }
+    // 解析命令行参数，忽略错误
+    _ = fs.Parse(os.Args[1:])
 
     // 打印所有输入的参数
-    fmt.Printf("输入文件: %s\n", inputFile)
-    fmt.Printf("输出目录: %s\n", outputDir)
+    fmt.Printf("输入文件路径: %s\n", inputPath)
+    fmt.Printf("输出文件目录: %s\n", outputDir)
 
-    // 打印未解析的参数
-    if len(flag.Args()) > 0 {
-        fmt.Println("未解析的参数:", flag.Args())
-    }
-
-    // 主程序逻辑
+    // 这里可以添加原来 main 函数的其他逻辑
     fmt.Println("开始处理域名...")
 
-    // 这里添加您的主要处理逻辑
-    // ...
+    // ... (其他代码逻辑)
 
     fmt.Println("处理完成。")
 }
