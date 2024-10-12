bc3c42dbf12cf30850b70272f062ec5a036de03c
blackfeather527 <blackfeather527@gmail.com>
2024-10-13 00:31:29 +0800

    Update process_domains.go

--- process_domains.go
+++ process_domains.go
@@ -167,6 +167,10 @@ func checkDomains(domains *sync.Map) *sync.Map {
     return validDomains
 }
 
+type Config struct {
+    Proxies map[string]any  `yaml:"proxies" json:"proxies"`
+}
+
 func fetchAndParseProxies(validDomains *sync.Map) *sync.Map {
     proxiesMap, stats := &sync.Map{}, struct{ total, unique, sites, duplicates, available, checked int64 }{}
     var wg sync.WaitGroup
@@ -205,7 +209,7 @@ func fetchAndParseProxies(validDomains *sync.Map) *sync.Map {
 	    }
 	    _ , err = c.ParseRawConfig(rawConfig)
 	    log.Printf("解析 %s 测试结果: %v", domain, err)
-            var config = Config{ Proxies: []map[string]any{} `yaml:"proxies"`, }
+            var config = Config{ Proxies: []map[string]any{}, }
             if err := yaml.Unmarshal(bodyBytes, config.Proxies); err != nil|| len(config.Proxies) == 0 {
                 log.Printf("解析 %s 的YAML失败或无有效代理: %v", domain, err)
                 return
